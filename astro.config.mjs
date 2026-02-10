af658cd94ca7c634ef4f9ee5e7573713a885e552
Jerard Pelaez <pelaezjerard@gmail.com>
2026-02-11 04:04:44 +0800

    add pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jerardpelaez.github.io',
+  base: '/portfolio-jerardpelaez',
   integrations: [vue()],
 
   vite: {
